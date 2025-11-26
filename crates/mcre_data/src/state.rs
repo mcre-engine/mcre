18b91b732a6254b230a4db01c56872824350a738
Dilshad <a-rustacean@outlook.com>
2025-11-26 20:15:51 +0530

    feat(crates/mcre_data): add test for random offset

--- crates/mcre_data/src/state.rs
+++ crates/mcre_data/src/state.rs
@@ -113,11 +113,139 @@ impl BlockState {
 
 #[cfg(test)]
 mod tests {
-    use crate::state::BlockState;
+    use jni::{JNIEnv, objects::JValueGen};
+    use mcre_core::BlockPos;
+
+    use crate::state::{BlockState, OffsetType};
 
     #[tokio::test]
     async fn test_block_state_data_load() {
         let block_states = BlockState::all().await.unwrap();
         assert!(!block_states.is_empty());
+    }
+
+    #[mcje::test]
+    async fn test_random_offset(env: &mut JNIEnv<'_>) {
+        let block_states = BlockState::all().await.unwrap();
+
+        let block_state_registry = env
+            .get_static_field(
+                "net/minecraft/world/level/block/Block",
+                "BLOCK_STATE_REGISTRY",
+                "Lnet/minecraft/core/IdMapper;",
+            )
+            .unwrap()
+            .l()
+            .unwrap();
+
+        let mut block_state = env
+            .call_method(
+                &block_state_registry,
+                "byId",
+                "(I)Ljava/lang/Object;",
+                &[JValueGen::Int(0)],
+            )
+            .unwrap()
+            .l()
+            .unwrap();
+
+        let mut block_state_id = 0u16;
+
+        while !block_state.is_null() {
+            let offset_function = env
+                .get_field(
+                    &block_state,
+                    "offsetFunction",
+                    "Lnet/minecraft/world/level/block/state/BlockBehaviour$OffsetFunction;",
+                )
+                .unwrap()
+                .l()
+                .unwrap();
+            if offset_function.is_null() {
+                assert_eq!(
+                    block_states[block_state_id as usize].offset_type,
+                    OffsetType::None
+                );
+            } else {
+                let block_pos_class = "net/minecraft/core/BlockPos";
+
+                for i in 0..10 {
+                    // BlockPos(i, i, i)
+                    let pos_obj = env
+                        .new_object(
+                            block_pos_class,
+                            "(III)V",
+                            &[JValueGen::Int(i), JValueGen::Int(i), JValueGen::Int(i)],
+                        )
+                        .unwrap();
+
+                    // Call offsetFunction.evaluate(state, pos)
+                    // Signature: (LBlockState;LBlockPos;)LVec3;
+                    let vec3_obj = env.call_method(
+                        &offset_function,
+                        "evaluate",
+                        "(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/phys/Vec3;",
+                        &[JValueGen::Object(&block_state), JValueGen::Object(&pos_obj)]
+                    ).unwrap().l().unwrap();
+
+                    let java_x = env.get_field(&vec3_obj, "x", "D").unwrap().d().unwrap();
+                    let java_y = env.get_field(&vec3_obj, "y", "D").unwrap().d().unwrap();
+                    let java_z = env.get_field(&vec3_obj, "z", "D").unwrap().d().unwrap();
+
+                    let java_value = (java_x, java_y, java_z);
+
+                    let block_state = &block_states[block_state_id as usize];
+
+                    let rust_value = block_state.random_offset(BlockPos::new(i, i, i));
+
+                    assert!(
+                        (rust_value.0 - java_value.0).abs() < 1e-6,
+                        "State: {:#?}\nBlockPos({i}, {i}, {i})\nOffset:\n  Rust: ({}, {}, {})\n  Java: ({}, {}, {})",
+                        block_state,
+                        rust_value.0,
+                        rust_value.1,
+                        rust_value.2,
+                        java_value.0,
+                        java_value.1,
+                        java_value.2
+                    );
+
+                    assert!(
+                        (rust_value.1 - java_value.1).abs() < 1e-6,
+                        "State: {:#?}\nBlockPos({i}, {i}, {i})\nOffset:\n  Rust: ({}, {}, {})\n  Java: ({}, {}, {})",
+                        block_state,
+                        rust_value.0,
+                        rust_value.1,
+                        rust_value.2,
+                        java_value.0,
+                        java_value.1,
+                        java_value.2
+                    );
+
+                    assert!(
+                        (rust_value.2 - java_value.2).abs() < 1e-6,
+                        "State: {:#?}\nBlockPos({i}, {i}, {i})\nOffset:\n  Rust: ({}, {}, {})\n  Java: ({}, {}, {})",
+                        block_state,
+                        rust_value.0,
+                        rust_value.1,
+                        rust_value.2,
+                        java_value.0,
+                        java_value.1,
+                        java_value.2
+                    );
+                }
+            }
+            block_state_id += 1;
+            block_state = env
+                .call_method(
+                    &block_state_registry,
+                    "byId",
+                    "(I)Ljava/lang/Object;",
+                    &[JValueGen::Int(block_state_id.into())],
+                )
+                .unwrap()
+                .l()
+                .unwrap();
+        }
     }
 }
